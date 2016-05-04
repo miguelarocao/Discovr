94c7ee8e2e82f1a474ac91dcfd1ed2810a48053d
miguelarocao <mig_ao@live.com>
2016-05-03 23:15:43 -0700

    Modified json format for tweet reader to allow for lazy reading. Parsed newest data.

--- tweet_plot.R
+++ tweet_plot.R
@@ -1,16 +1,17 @@
 #Load data
 setwd("C:\\Users\\Miguel\\Dropbox\\Caltech_2015-2016\\Spring\\EE_145\\discovr_code\\pair_data")
-act = read.csv("output_pairs20160425-192014.txt",header=FALSE)
-names(act)<-c("activity_1","activity_2","matches")
+act = read.csv("output_pairs20160503-224536.txt",header=FALSE)
+names(act)<-c("primary","secondary","matches")
 
 #normalize
-act=act[order(act$activity_1,act$activity_2),]
+act=act[order(act$primary,act$secondary),]
 num_pair=numeric()
-for(activity in unique(act$activity_1))
+for(activity in unique(act$primary))
 {
-  num_pair=append(num_pair,length(which(act$activity_1==activity)))
+  num_pair=append(num_pair,length(which(act$primary==activity)))
 }
-act.counts=act[which(act$activity_1==act$activity_2),]$matches
+act.counts=act[which(act$primary==act$secondary),]$matches
+
 act.counts=rep(act.counts,num_pair)
 
 act.matrix=as.matrix(as.numeric(act$matches))
@@ -19,20 +20,21 @@ act.norm=sweep(act.matrix,1,act.counts,"/")
 
 act$normalized<-act.norm
 
-#only take top 20 activities
-act.unique=act[which(act$activity_1==act$activity_2 & is.finite(act$normalized)),]
+#only take top 20 activities (in order)
+act.unique=act[which(act$primary==act$secondary & is.finite(act$normalized)),]
 act.unique.sort=act.unique[order(-act.unique$matches),]
-act.short.names=factor(head(act.unique.sort,20)$activity_1)
-act.short=act[which(is.element(act$activity_1,act.short.names) & is.element(act$activity_2,act.short.names)),]
-act.short$activity_1<-factor(act.short$activity_1)
-act.short$activity_2<-factor(act.short$activity_2)
+act.short.names=factor(head(act.unique.sort,20)$primary)
+act.short=act[which(is.element(act$primary,act.short.names) & is.element(act$secondary,act.short.names)),]
+act.short$primary<-factor(act.short$primary)
+act.short$secondary<-factor(act.short$secondary)
 
 #get desired form
-act.corr=xtabs(act.short$normalized ~ act.short$activity_1 + act.short$activity_2, act.short)
+act.corr=xtabs(act.short$normalized ~ act.short$primary + act.short$secondary, act.short)
 
 #plot
 library(corrplot)
 
-M <- cor(act.corr)
-corrplot(M,method="color",type="upper",tl.srt=45)
+#M <- cor(act.corr)
+custom_col=colorRampPalette(c("white","white","red"))
+corrplot(act.corr,method="color",order="FPC",tl.srt=90,cl.lim=c(0,1),tl.cex=1)
   
--- tweet_reader.py
+++ tweet_reader.py
@@ -5,10 +5,6 @@ import time
 import json
 import sys
 
-#TO DO: fix hockey and field hockey correlation and basketball vs BBall
-#TO DO: look at hockey and field hockey pair, weird correlation
-#TO DO: Parse new json format
-
 #confidence of matchign with a word
 FUZZ_CONFIDENCE=90
 
@@ -32,20 +28,20 @@ def main():
     user_count=0
     user_count=0
     start_time=time.clock()
-    curr_user=None
     user_tweets=[]
     for activity in activity_list:
-        filename="tweet_by_activity/old/output_"+activity+".txt" #TODO: Change back
+        filename="tweet_by_activity/output_"+activity+".txt"
+        curr_user=None
         with open(filename,'r') as f:
-            tweet_list=json.load(f)
-            curr_user=tweet_list[0]["user"]
-            user_list=[]
-            for tweet in tweet_list:
-                if (str(tweet['user'])!=curr_user or
-                    len(user_tweets)>=MAX_TWEET or tweet_count==(len(tweet_list)-1)):
+            for line in f:
+                tweet=json.loads(line)
+                if not curr_user:
+                    #initialization
+                    curr_user=tweet['user']
+                elif str(tweet['user'])!=curr_user:
                     user_pop_dict(activity,userfile,curr_user,act_dict,user_tweets)
-                    print str(user_count)+" ("+activity+"): "+curr_user+" "+str(len(user_tweets))
                     #print user_tweets[0]
+                    print str(user_count)+" ("+activity+"): "+curr_user+" "+str(len(user_tweets))
                     user_count+=1
                     user_tweets=[]
                     curr_user=tweet['user']
@@ -53,13 +49,16 @@ def main():
                 user_tweets.append(tweet["text"])
                 tweet_count+=1
 
+            #done getting lines, parse last user set
+            user_pop_dict(activity,userfile,curr_user,act_dict,user_tweets)
+            print str(user_count)+" ("+activity+"): "+curr_user+" "+str(len(user_tweets))
+
         f.close()
         user_count=0
 
     write_out_pairs(act_dict)
 
     print "Num tweets: "+str(tweet_count)
-    print "Num users: "+str(user_count)
     print "Time: "+str(time.clock()-start_time)
 
 
