014a331354391261dbb02a437c7c1b4bb13fe1ba
Akshta <aathawal@caltech.edu>
2016-05-05 16:13:35 -0700

    adding the code for reccomendation

---
+++ recommend.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+import json
+
+with open('weights.json', 'r') as fp:
+   weights = json.load(fp)
+
+activs = [
+'Bowling',
+'Dance',
+'Disc Golf',
+'Go Kart',
+'Laser Tag',
+'Mini Golf',
+'Golf',
+'Swimming (sport)',
+'Salsa (dance)',
+'Samba',
+'Baseball',
+'Basketball',
+'Cricket',
+'American football',
+'Hockey',
+'Racquetball',
+'Association football',
+'Table Tennis',
+'Tennis',
+'Track and field',
+'Running',
+'Badminton',
+'Curling',
+'Boxing',
+'Physical fitness',
+'Sport Climbing',
+'Rock Climbing',
+'Cycling',
+'Gymnastics',
+'Martial Arts',
+'Yoga',
+'Paddleboarding',
+'Field Hockey',
+'Paintball',
+'Rugby football',
+'Skateboarding',
+'Inline skating',
+'Roller skating',
+'Squash (sport)',
+'Surfing',
+'Motorcycling',
+'Hiking',
+'Rafting',
+'Skiing',
+'Horseback Riding',
+'Volleyball',
+'Fencing',
+'Parkour',
+'Trampolining',
+'Skydiving',
+'Bungee jumping',
+'Ziplining',
+'Snowboarding',
+'Softball',
+'Archery ',
+'Shooting sport',
+'Racing',
+'Billiards',
+'Pool (cue sports)',
+'Paragliding',
+'Handball',
+'Pottery',
+'Photography',
+'Painting',
+'Drawing',
+'Singing',
+'Improvisational theatre',
+'Comedy',
+'Theatre',
+'Origami',
+'Sculpture',
+'Karaoke',
+'Charity shop',
+'Reading (process)',
+'Escape room',
+'Gambling ',
+'Arcade game',
+]
+
+ip = [
+'Motorcycling'
+]
+
+for elem in ip:
+    id = activs.index(elem)
+    del activs[id]
+    
+scores = dict()
+for elem in activs:
+    scores[elem] = 0
+
+for key in ip:
+    for elem in activs:
+        scores[elem] += weights[key][elem]
+    
+factor = sum(scores.itervalues())    
+for key in scores:
+    scores[key] /= factor
+
+for activity in ip:
+    scores.pop(activity, None)
+
+print sorted(scores, key=scores.get, reverse=True)[:5]
+
+
