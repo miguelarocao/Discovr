ef14e1296161784a0b02567100cd8955871c56ee
miguelarocao <mig_ao@live.com>
2016-05-16 13:20:58 -0700

    finishe main web page

--- web/index.php
+++ web/index.php
@@ -1,97 +1,124 @@
-<form method="post" action="recommend.php">
-What activities do you like?
-<div class="input_fields_wrap">
-<?php
-$activities = array(
-'',
-'Bowling',
-'Dance',
-'Disc Golf',
-'Go Kart',
-'Laser Tag',
-'Mini Golf',
-'Golf',
-'Swimming (sport)',
-'Salsa (dance)',
-'Samba',
-'Baseball',
-'Basketball',
-'Cricket',
-'American football',
-'Hockey',
-'Racquetball',
-'Association football',
-'Table Tennis',
-'Tennis',
-'Track and field',
-'Running',
-'Badminton',
-'Curling',
-'Boxing',
-'Physical fitness',
-'Sport Climbing',
-'Rock Climbing',
-'Cycling',
-'Gymnastics',
-'Martial Arts',
-'Yoga',
-'Paddleboarding',
-'Field Hockey',
-'Paintball',
-'Rugby football',
-'Skateboarding',
-'Inline skating',
-'Roller skating',
-'Squash (sport)',
-'Surfing',
-'Motorcycling',
-'Hiking',
-'Rafting',
-'Skiing',
-'Horseback Riding',
-'Volleyball',
-'Fencing',
-'Parkour',
-'Trampolining',
-'Skydiving',
-'Bungee jumping',
-'Ziplining',
-'Snowboarding',
-'Softball',
-'Archery ',
-'Shooting sport',
-'Racing',
-'Billiards',
-'Pool (cue sports)',
-'Paragliding',
-'Handball',
-'Pottery',
-'Photography',
-'Painting',
-'Drawing',
-'Singing',
-'Improvisational theatre',
-'Comedy',
-'Theatre',
-'Origami',
-'Sculpture',
-'Karaoke',
-'Charity shop',
-'Reading (process)',
-'Escape room',
-'Gambling ',
-'Arcade game',
-);
+<link rel="stylesheet" type = "text/css" href="style.css">
 
-for($i=0;$i<5;$i++)
-{
-    echo '<div><select name="activities[]">';
-    foreach($activities as $act){
-        echo "<option>{$act}</option>";
-    }
-    echo '</select></div>';    
-}
-?>
-<input type="submit">
+<form method="post" action="recommend.php">
+<div class="discovr">
+discovr
 </div>
+<div class="center">
+    <div id="prompt">
+        <div id="big_prompt">
+        What Are Your 5 Favorite Activities?
+        </div>
+        <div id="small_prompt">
+        Help us get to know you a little better.
+        </div>
+     </div>
+    <div class="input_wrap">
+        <?php
+        $activities = array(
+        '', 
+        'Bowling', /*Indoor Sports*/
+        'Dance',
+        'Golf',
+        'Swimming (sport)',
+        'Salsa (dance)',
+        'Samba',
+        'Basketball',
+        'Hockey',
+        'Racquetball',
+        'Table Tennis',
+        'Badminton',
+        'Curling',
+        'Boxing',
+        'Physical fitness',
+        'Gymnastics',
+        'Martial Arts',
+        'Yoga',
+        'Squash (sport)',
+        'Volleyball',
+        'Fencing',
+        'Trampolining',
+        'Handball',
+        'Surfing', /*Outdoor Sports*/
+        'Baseball',
+        'Disc Golf',
+        'Cricket',
+        'Association football',
+        'American football',
+        'Tennis',
+        'Track and field',
+        'Running',
+        'Sport Climbing',
+        'Rock Climbing',
+        'Cycling',
+        'Paddleboarding',
+        'Field Hockey',
+        'Paintball',
+        'Rugby football',
+        'Skateboarding',
+        'Inline skating',
+        'Roller skating',
+        'Hiking',
+        'Rafting',
+        'Skiing',
+        'Horseback Riding',
+        'Parkour',
+        'Skydiving',
+        'Snowboarding',
+        'Softball',
+        'Archery ',
+        'Shooting sport',
+        'Racing',
+        'Paragliding',
+        'Pottery', /*Arts*/
+        'Photography',
+        'Painting',
+        'Drawing',
+        'Singing',
+        'Improvisational theatre',
+        'Comedy',
+        'Theatre',
+        'Origami',
+        'Sculpture',
+        'Karaoke',
+        'Charity shop', /*Other*/
+        'Billiards',
+        'Motorcycling',
+        'Reading (process)',
+        'Pool (cue sports)',
+        'Go Kart',
+        'Laser Tag',
+        'Mini Golf',
+        'Escape room',
+        'Gambling ',
+        'Arcade game',
+        'Bungee jumping',
+        'Ziplining',
+        );
+        $labels = array (
+            1 => "Indoor Sports",
+            23 => "Outdoor Sports",
+            54 => "Arts",
+            65 => "Other",
+        );
+        for($i=0;$i<5;$i++)
+        {
+            echo '<div><select name="activities[]" id="activities">';
+            for($j=0;$j<count($activities);$j++)
+            {
+                if(array_key_exists ($j, $labels))
+                {
+                    if($j!=0)
+                        echo "</optgroup>";
+                    echo "<optgroup label={$labels[$j]}>";
+                }
+                echo "<option>{$activities[$j]}</option>";
+            }
+            echo '</optgroup></select></div>';    
+        }
+        ?>
+    </div>
+    
+</div>
+<input type="submit" class="submit" value="Try Something New!">
 </form>
