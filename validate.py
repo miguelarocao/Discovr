72c9816782905a403095a37fa4c089bc3920b0bb
Mannat Singh <mssingh@caltech.edu>
2016-05-20 03:46:28 -0700

    Added code to plot results and modified validation script

--- validate.py
+++ validate.py
@@ -3,8 +3,14 @@ import json
 import sys
 import copy
 
-with open('weights.json', 'r') as fp:
-   weights = json.load(fp)
+#with open('weights.json', 'r') as fp:
+#   weights = json.load(fp)
+
+with open('result_wiki.json', 'r') as fp:
+   wiki_data = json.load(fp)
+   
+with open('result_tweet.json', 'r') as fp:
+   tweet_data = json.load(fp)
 
 #fail = open('failures.txt', 'w')
 
@@ -92,7 +98,7 @@ fail_count = dict()
 for act in activs:
     fail_count[act] = [0, 0]
 
-def recommend(ip):
+def recommend(ip, weights):
     # remove duplicate inputs
     ip_set = set(ip)
     ip = list(ip_set)
@@ -123,7 +129,7 @@ def recommend(ip):
     
     return sorted(scores, key=scores.get, reverse=True)
     
-def validate(ip):
+def validate(ip, size, weights):
     # remove duplicate inputs
     ip_set = set(ip)
     ip = list(ip_set)
@@ -133,10 +139,10 @@ def validate(ip):
     for i in range(len(ip)):
         act_list = copy.deepcopy(ip)
         del act_list[i]
-        res = recommend(act_list)
+        res = recommend(act_list, weights)
         rank = res.index(ip[i])
         fail_count[ip[i]][0] += 1
-        if rank < 5:
+        if rank < size:
             pred += 1
         else:
             fail_count[ip[i]][1] += 1
@@ -147,23 +153,57 @@ def validate(ip):
         
     return [sum_rank, pred, count]
 
-with open('pair_data/val/val_subsample_mod.txt') as f:
-    count = 0
-    sum_pred = 0
-    sum_rank = 0
-    norm_sum_pred = 0
-    norm_count = 0
-    for line in f:
-        line = line[0:-1]
-        ip = line.split(",")
-        [sum_r, pred, cnt] = validate(ip)
-        sum_rank += sum_r
-        sum_pred += pred
-        count += cnt
-        norm_sum_pred += float(pred)/cnt
-        norm_count += 1
-    avg_rank = float(sum_rank)/count
-    avg_pred = float(sum_pred)/count
-    avg_norm_pred = float(norm_sum_pred)/norm_count
+def get_weights(alpha):
+    weights = dict()
 
+    for key in wiki_data:
+        weights[key] = dict()
+        for key2 in wiki_data[key]:
+            weights[key][key2] = (1-alpha)*wiki_data[key][key2] + alpha*tweet_data[key][key2]
+    return weights
+
+perf = dict()
+
+for i in range(0, 20):
+    size = i + 1
+    perf[size] = dict()
+    for alpha in range(0, 11):
+        alpha = float(alpha)/10
+        w = get_weights(alpha)
+
+        with open('pair_data/val/val_subsample_mod.txt') as f:
+            count = 0
+            sum_pred = 0
+            sum_rank = 0
+            norm_sum_pred = 0
+            norm_count = 0
+            for line in f:
+                line = line[0:-1]
+                ip = line.split(",")
+                [sum_r, pred, cnt] = validate(ip, size, w)
+                sum_rank += sum_r
+                sum_pred += pred
+                count += cnt
+                norm_sum_pred += float(pred)/cnt
+                norm_count += 1
+            avg_rank = float(sum_rank)/count
+            avg_pred = float(sum_pred)/count
+            avg_norm_pred = float(norm_sum_pred)/norm_count
+            perf[size][alpha] = avg_norm_pred
+            print "size = " + str(size) + ", alpha = " + str(alpha) + ", perf = " + str(avg_norm_pred)
 #fail.close()
+
+max_perf = []
+for i in range(0, 20):
+    size = i+1
+    max_i = max(perf[size].itervalues())
+    max_perf.append(max_i)
+
+print max_perf
+
+alpha_perf = []
+for alpha in range(0, 11):
+    alpha = float(alpha)/10
+    alpha_perf.append(perf[5][alpha])
+
+print alpha_perf
